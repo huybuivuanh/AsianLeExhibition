6b17b2e5b1cb2671e5d5644e0b4bba4d15859e65
huybuivuanh <huy_buivu12345@yahoo.ca>
2025-07-22 10:10:40 -0600

    anonymous sign in

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getAuth, signInAnonymously } from '@react-native-firebase/auth';
 import { Provider } from 'react-redux';
 import { Store } from '../Redux/Store';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -9,18 +10,33 @@ import { LiveOrderDataProvider } from './LiveOrderDataProvider';
 import { OrderHistoryDataProvider } from './OrderHistoryDataProvider';
 import { SalesDataProvider } from './SalesDataProvider';
 
-export const AppProviders = ({ children }: { children: React.ReactNode }) => (
-  <Provider store={Store}>
-    <GestureHandlerRootView>
-      <NavigationContainer>
-        <MenuDataProvider>
-          <LiveOrderDataProvider>
-            <OrderHistoryDataProvider>
-              <SalesDataProvider>{children}</SalesDataProvider>
-            </OrderHistoryDataProvider>
-          </LiveOrderDataProvider>
-        </MenuDataProvider>
-      </NavigationContainer>
-    </GestureHandlerRootView>
-  </Provider>
-);
+export const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    async function loginAnon() {
+      try {
+        const authInstance = getAuth();
+        const userCredential = await signInAnonymously(authInstance);
+        console.log('Signed in anonymously:', userCredential.user.uid);
+      } catch (error) {
+        console.error('Anonymous sign-in error:', error);
+      }
+    }
+
+    loginAnon();
+  }, []);
+  return (
+    <Provider store={Store}>
+      <GestureHandlerRootView>
+        <NavigationContainer>
+          <MenuDataProvider>
+            <LiveOrderDataProvider>
+              <OrderHistoryDataProvider>
+                <SalesDataProvider>{children}</SalesDataProvider>
+              </OrderHistoryDataProvider>
+            </LiveOrderDataProvider>
+          </MenuDataProvider>
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </Provider>
+  );
+};
